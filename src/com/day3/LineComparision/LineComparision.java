130143752490e9adede9480293c67acf6505eefb
ravi406 <ravikamble406@gmail.com>
2022-11-25 14:44:54 +0530

    added welcome message to the line comparision program

---
+++ src/com/day3/LineComparision/LineComparision.java
@@ -0,0 +1,10 @@
+package com.day3.LineComparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparision Computation :");
+	}
+
+}
