9838acaff88762471089c5766db9a539d595c9ec
ravi406 <ravikamble406@gmail.com>
2022-11-27 11:03:33 +0530

    Added another line And checked both lines are equal or not using equal method

--- src/com/day3/LineComparision/LineComparision.java
+++ src/com/day3/LineComparision/LineComparision.java
@@ -1,4 +1,4 @@
-package com.day3.LineComparision;
+package com.day3.linecomparision;
 
 import java.util.Scanner;
 
@@ -19,8 +19,27 @@ public class LineComparision {
 		int y2 = scanner.nextInt();
 
 		System.out.println("A Length as 2 points : " + "(" + x1 + "," + y1 + ")" + "and" + "(" + x2 + "," + y2 + ")");
-		double lineLength = Math.sqrt((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
-		System.out.println("Length of a line : " + lineLength);
+		double lineLength1 = Math.sqrt((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+		System.out.println("Length of a line : " + lineLength1);
+		String str1 = Double.toString(lineLength1);
+
+		System.out.println("Enter the co ordinates of x3 and y3 : ");
+		int x3 = scanner.nextInt();
+		int y3 = scanner.nextInt();
+
+		System.out.println("Enter the co ordinates of x4 and y4 : ");
+		int x4 = scanner.nextInt();
+		int y4 = scanner.nextInt();
+
+		System.out.println("A Length as 2 points : " + "(" + x3 + "," + y3 + ")" + "and" + "(" + x4 + "," + y4 + ")");
+		double lineLength2 = Math.sqrt((x4 - x3) * (x4 - x3)) + ((y4 - y3) * (y4 - y3));
+		System.out.println("Length of a line : " + lineLength2);
+		String str2 = Double.toString(lineLength2);
+
+		if (str1.equals(str2)) {
+			System.out.println("Both lines are EQUAL ");
+		} else
+			System.out.println("Both lines are Not EQUAL ");
 
 		scanner.close();
 
